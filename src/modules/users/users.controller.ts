24c335f475e5440975d1b1cc7a5092154f31d7a1
Alejandro E Abalos F <alejandroexe96@gmail.com>
2024-05-09 22:34:32 -0300

    task-CRUD-User

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from '../auth/dto/create-auth.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getUsers(@Query('page') page: number = 1, @Query('limit') limit: number = 3) {
+    return this.usersService.getUsers(Number(page), Number(limit));
+  }
+
+  @Get(':id')
+  getUser(@Param('id', ParseUUIDPipe) id: string) {
+    return this.usersService.getUser(id);
+  }
+  @Put('update/:id')
+  updateUser(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateUserDto: Partial<CreateUserDto>,
+  ) {
+    console.log(id, updateUserDto);
+    return this.usersService.updateUser(id, updateUserDto);
+  }
+
+  @Put('unsubscribe/:id')
+  unsubscribeUser(@Param('id', ParseUUIDPipe) id: string) {
+    return this.usersService.unsubscribeUser(id);
+  }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -13,6 +13,115 @@ export class UsersService {
     private readonly dataSource: DataSource,
   ) {}
 
+  async getUsers(page: number, limit: number) {
+    const start = (page - 1) * limit;
+    const end = start + limit;
+    return await this.userService.find({
+      skip: start,
+      take: end,
+      select: [
+        'username',
+        'name',
+        'lastName',
+        'document',
+        'phone',
+        'cellphone',
+        'email',
+        'image',
+        'state',
+        'rol',
+        'createdAt',
+        'lastLogin',
+      ],
+    });
+  }
+
+  async getUser(id: string) {
+    const userExists = await this.userService.find({
+      where: { id: id },
+      select: [
+        'username',
+        'name',
+        'lastName',
+        'document',
+        'phone',
+        'cellphone',
+        'email',
+        'image',
+        'rol',
+        'createdAt',
+        'lastLogin',
+      ],
+    });
+    if (!userExists) {
+      throw new HttpException('Usuario no encontrado', HttpStatus.NOT_FOUND);
+    }
+    return userExists;
+  }
+
+  async updateUser(id: string, updateUserDto: Partial<User>) {
+    const userExists = await this.userService.findOneBy({ id: id });
+    if (!userExists) {
+      throw new HttpException('Usuario no encontrado', HttpStatus.NOT_FOUND);
+    }
+    // console.log(userExists);
+    const queryRunner = await this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      userExists.password = updateUserDto?.password
+        ? await bcrypt.hash(updateUserDto.password, 10)
+        : userExists.password;
+      // userExists.email = updateUserDto.email;
+      userExists.username = updateUserDto?.username
+        ? updateUserDto?.username
+        : userExists.username;
+      userExists.name = updateUserDto?.name
+        ? updateUserDto?.name
+        : userExists.name;
+      userExists.lastName = updateUserDto?.lastName
+        ? updateUserDto?.lastName
+        : userExists.lastName;
+      userExists.document = updateUserDto?.document
+        ? updateUserDto?.document
+        : userExists.document;
+      userExists.phone = updateUserDto?.phone
+        ? updateUserDto?.phone
+        : userExists.phone;
+      userExists.cellphone = updateUserDto?.cellphone
+        ? updateUserDto?.cellphone
+        : userExists.cellphone;
+      // userExists.image = updateUserDto?.image;
+      userExists.email = updateUserDto?.email
+        ? updateUserDto?.email
+        : userExists.email;
+      console.log(userExists);
+      const userModified = await queryRunner.manager.save(userExists);
+      await queryRunner.manager.save(userModified);
+      await queryRunner.commitTransaction();
+      const registerOkMessage = {
+        message: `Usuario actualizado correctamente`,
+      };
+
+      return registerOkMessage;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  async unsubscribeUser(id: string) {
+    const userExists = await this.userService.findOneBy({ id: id });
+    if (!userExists) {
+      throw new HttpException('Usuario no encontrado', HttpStatus.NOT_FOUND);
+    }
+    userExists.state = false;
+    await this.userService.save(userExists);
+    return 'El usuario fue dado de baja';
+  }
   async searchEmail(email: string) {
     return await this.userService.findOne({ where: { email: email } });
   }
