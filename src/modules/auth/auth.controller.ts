3e9ce158d25a4923dbc27cb5c0731e911261ec14
JPRuedaZ <juanpabloruedazo@gmail.com>
2024-05-20 12:21:27 -0500

    actualizando respuestas en la documentacion

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -14,18 +14,64 @@ import { LoginUserDto } from './dto/login-auth.dto';
 import { GoogleAuthGuard } from '../../guards/google-guard.guard';
 import { Request } from 'express';
 import { Response } from 'express';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Authentications')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+  @ApiResponse({
+    status: 201,
+    schema: {
+      example: {
+        username: 'juanperez14',
+        password: 'Password123!',
+        name: 'Juan Pérez',
+        lastName: 'Juan Pérez',
+        document: 12345678,
+        phone: 1234567890,
+        cellphone: 1234567890,
+        email: 'usuario@example.com',
+        code: 'codigo de la vivienda proporcionado por el admin ',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Ya existe un usuario registrado con ese email.',
+  })
   @Post('signup')
   signUpUser(@Body() createUserDto: CreateUserDto) {
     return this.authService.signUpUser(createUserDto);
   }
 
+  @ApiResponse({
+    status: 200,
+    schema: {
+      example: {
+        token:
+          'eyGhyGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+        user: {
+          id: '850c3059-1314-4eab-8a9e-1fe88d32ed13',
+          username: 'juanperez14',
+          name: 'Juan',
+          lastName: 'Perez',
+          document: 123456789,
+          image:
+            'https://res.cloudinary.com/dcqdilhek/image/upload/fl_preserve_transparency/v1715136207/zmuncvwsnlws77vegwxq.jpg',
+          phone: null,
+          cellphone: 123456789,
+          email: 'usuario@example.com',
+          rol: 'owner',
+          lastLogin: '2024-05-18T01:33:25.027Z',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Algun dato ingresado es incorrecto',
+  })
   @HttpCode(200)
   @Post('signin')
   signInUser(@Body() userLogin: LoginUserDto) {
