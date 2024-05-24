310534bbd3ac711294189594881ce40aceecd897
Cesar Ausa <cesarausaprog@gmail.com>
2024-05-24 14:39:36 -0300

    authorizations path updated

--- src/modules/authorizations/dto/update-authorization.dto.ts
+++ src/modules/authorizations/dto/update-authorization.dto.ts
@@ -1,11 +1,7 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAuthorizationDto } from './create-authorization.dto';
-import { IsNumber, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateAuthorizationDto extends PartialType(
-  CreateAuthorizationDto,
-) {
-  @IsOptional()
-  @IsNumber()
-  number?: number;
+export class UpdateAuthorizationDto {
+  @IsNotEmpty()
+  @IsString()
+  code: string;
 }
